3bd0c041901b3869457fe346eb5fa6fce77e31f5
lab314-10@labeng.local <lab314-10@labeng.local>
2020-01-28 16:29:20 +0700

    finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,19 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+
+#include<fstream>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("C:\\Users\\lab314-10\\Documents\\lab9\\lab9-2562-2-wadeepat\\cheerbook.txt");
+    string text;
+    ofstream source1("C:\\Users\\lab314-10\\Documents\\lab9\\lab9-2562-2-wadeepat\\cheerbook_copy.txt");
+    source1<<"-------------------- SOTUS ---------------------\n";
+    while(getline(source,text)){
+        source1<<text<<"\n";
+    }
+    source1<<"-------------------- SOTUS ---------------------";
+    source.close();
+    source1.close();
+}
